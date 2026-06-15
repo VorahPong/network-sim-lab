081444ddee67ee255afe22e086bc0f4e0a2687b1
Vorahpong Mean <vorahpongmean@gmail.com>
2026-06-14 23:58:46 -0500

    Setup CMake C++

---
+++ build/CMakeFiles/sim_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim_node.dir/src/sim_node.cpp.o"
+  "CMakeFiles/sim_node.dir/src/sim_node.cpp.o.d"
+  "sim_node"
+  "sim_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/sim_node.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Network Simulation Lab" << std::endl;
+    return 0;
+
+    return 0;
+}
